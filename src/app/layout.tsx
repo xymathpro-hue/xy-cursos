a0ddcb95c8148ffb25ef9ab7efa16daecbdd5392
xymathpro-hue <xymathpro@gmail.com>
2025-12-08 10:48:42 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link 
+          rel="stylesheet" 
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css" 
+          integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV" 
+          crossOrigin="anonymous"
+        />
+        <script 
+          defer 
+          src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js" 
+          integrity="sha384-XjKyOOlGwcjNTAIQHIpgOno0Ber8vR0c9Z1EqpfQrj+MkPJBwU+TN9LH6PAI5u7R" 
+          crossOrigin="anonymous"
+        />
+        <script 
+          defer 
+          src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/auto-render.min.js" 
+          integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" 
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="antialiased">{children}</body>
     </html>
   );
