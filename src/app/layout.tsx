c2e66b4aff163efecedf4e7713d158ef8adc45c6
xymathpro-hue <xymathpro@gmail.com>
2025-12-08 11:34:13 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,17 +6,9 @@ export const metadata: Metadata = {
     default: 'XY Cursos | Plataforma Integrada de Estudos',
     template: '%s | XY Cursos',
   },
-  description: 'Sua aprovação começa aqui e agora. Três plataformas especializadas para você conquistar seus objetivos: ENEM, Olimpíadas de Matemática e IFPI.',
-  keywords: ['ENEM', 'Matemática', 'Olimpíadas', 'IFPI', 'Preparatório', 'Estudos', 'Educação'],
+  description: 'Sua aprovação começa aqui e agora.',
+  keywords: ['ENEM', 'Matemática', 'Olimpíadas', 'IFPI', 'Preparatório'],
   authors: [{ name: 'XY Cursos' }],
-  openGraph: {
-    title: 'XY Cursos | Plataforma Integrada de Estudos',
-    description: 'Sua aprovação começa aqui e agora.',
-    url: 'https://xycursos.com.br',
-    siteName: 'XY Cursos',
-    locale: 'pt_BR',
-    type: 'website',
-  },
 };
 
 export default function RootLayout({
@@ -27,23 +19,24 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <link 
-          rel="stylesheet" 
-          href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css" 
-          integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV" 
-          crossOrigin="anonymous"
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.MathJax = {
+                tex: {
+                  inlineMath: [['$', '$']],
+                  displayMath: [['$$', '$$']],
+                },
+                svg: {
+                  fontCache: 'global'
+                }
+              };
+            `,
+          }}
         />
-        <script 
-          defer 
-          src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js" 
-          integrity="sha384-XjKyOOlGwcjNTAIQHIpgOno0Ber8vR0c9Z1EqpfQrj+MkPJBwU+TN9LH6PAI5u7R" 
-          crossOrigin="anonymous"
-        />
-        <script 
-          defer 
-          src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/auto-render.min.js" 
-          integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" 
-          crossOrigin="anonymous"
+        <script
+          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"
+          async
         />
       </head>
       <body className="antialiased">{children}</body>
