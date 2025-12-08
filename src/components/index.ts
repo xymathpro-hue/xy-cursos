b7182b1a2b342db7aebe5144ea803e1646402be4
xymathpro-hue <xymathpro@gmail.com>
2025-12-07 22:55:47 -0300

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
-
+// Exportar componentes
+export { DashboardStats } from './dashboard/DashboardStats';
+export { BatalhaRapida } from './batalha/BatalhaRapida';
+export { CadernoErros } from './caderno/CadernoErros';
+export { DicaDoDia } from './dicas/DicaDoDia';
